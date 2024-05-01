960b2cdb9f1107e9a64ba8a463db5213e659c7eb
Logan Roberts <logancroberts@outlook.com>
2024-05-01 16:16:50 -0400

    Clean up build.sh a bit and fix copr repo

--- build.sh
+++ build.sh
@@ -8,11 +8,9 @@ RELEASE="$(rpm -E %fedora)"
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# curl -Lo /etc/yum.repos.d/_copr_vineelsai-hypr.repo https://copr.fedorainfracloud.org/coprs/vineelsai/hypr/repo/fedora-40/vineelsai-hypr-fedora-40.repo
-curl -Lo /etc/yum.repos.d/_copr_vineelsai-hypr.repo https://copr.fedorainfracloud.org/coprs/vineelsai/hypr/repo/fedora-"${FEDORA_MAJOR_VERSION}"/vineelsai-hypr-fedora-"${FEDORA_MAJOR_VERSION}".repo
+# Add copr repos
+curl -Lo /etc/yum.repos.d/_copr_vineelsai-hypr.repo https://copr.fedorainfracloud.org/coprs/vineelsai/hypr/repo/fedora-"${RELEASE}"/vineelsai-hypr-fedora-"${RELEASE}".repo
 
 # this installs a package from fedora repos
 rpm-ostree install discord \
@@ -22,9 +20,5 @@ rpm-ostree install discord \
 	hypridle \
 	hyprpaper
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
+# Example for enabling a System Unit File
 systemctl enable podman.socket

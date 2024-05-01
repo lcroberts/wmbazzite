e45a52e96de82e95414d70808d60d1415ec4a96e
Logan Roberts <logancroberts@outlook.com>
2024-05-01 09:09:08 -0400

    Add hyprland stuff

--- build.sh
+++ build.sh
@@ -4,16 +4,21 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf copr enable vineelsai/hypr
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install discord \
+	hyprland \
+	hyprpicker \
+	hyprlock \
+	hypridle \
+	hyprpaper
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc

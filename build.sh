a76d8b4d6de92cbaaca4d280a139468bf36f0dca
Logan Roberts <logancroberts@outlook.com>
2024-05-21 10:07:59 -0400

    add docker

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ RELEASE="$(rpm -E %fedora)"
 # Add copr repos
 curl -Lo /etc/yum.repos.d/_copr_solopasha-hypr.repo https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-"${RELEASE}"/solopasha-hyprland-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/_copr_wezfurlong-wezterm.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${RELEASE}"/wezfurlong-wezterm-nightly-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 # this installs a package from fedora repos
 rpm-ostree --idempotent install \
@@ -41,12 +42,16 @@ rpm-ostree --idempotent install \
 	zoxide \
 	tmux \
 	iotop \
-	podman-docker \
-	podman-tui \
-	podman-compose \
 	podmansh \
 	wezterm \
-	stow
+	stow \
+	podman-tui \
+	podman-compose \
+	docker-ce \
+	docker-ce-cli \
+	docker-buildx-plugin \
+	docker-compose-plugin \
+	containerd.io
 
 # Hyprland Stuff
 rpm-ostree --idempotent install \
@@ -69,4 +74,5 @@ rpm-ostree --idempotent install \
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
+systemctl enable docker
 sudo systemctl disable tailscaled.service

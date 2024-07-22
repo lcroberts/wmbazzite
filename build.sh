ad42bb7f67273736cf5262beb4c58bd16dc7926f
Logan Roberts <logancroberts@outlook.com>
2024-07-21 20:15:07 -0400

    Remove unused packages

--- build.sh
+++ build.sh
@@ -10,49 +10,44 @@ RELEASE="$(rpm -E %fedora)"
 # Add copr repos
 curl -Lo /etc/yum.repos.d/_copr_solopasha-hypr.repo https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-"${RELEASE}"/solopasha-hyprland-fedora-"${RELEASE}".repo
 curl -Lo /etc/yum.repos.d/_copr_wezfurlong-wezterm.repo https://copr.fedorainfracloud.org/coprs/wezfurlong/wezterm-nightly/repo/fedora-"${RELEASE}"/wezfurlong-wezterm-nightly-fedora-"${RELEASE}".repo
-curl -Lo /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 # this installs a package from fedora repos
-# rpm-ostree --idempotent install \
-#     adobe-source-han-sans-jp-fonts \
-#     adobe-source-han-serif-jp-fonts \
-#     mozilla-fira-mono-fonts \
-#     powerline-fonts \
-#     jetbrains-mono-fonts-all \
-#     google-droid-sans-mono-fonts \
-#     rofi-wayland \
-#     kvantum \
-#     kvantum-qt5 \
-#     qt6ct \
-#     qt5ct \
-#     onedrive \
-#     zathura \
-#     zathura-pdf-mupdf \
-#     zathura-djvu \
-#     zathura-ps \
-#     zathura-cb \
-#     fcitx5 \
-#     fcitx5-configtool \
-#     fcitx5-gtk \
-#     fcitx5-qt5 \
-#     fcitx5-mozc \
-#     neovim \
-#     eza \
-#     ripgrep \
-#     zoxide \
-#     tmux \
-#     iotop \
-#     tokei \
-#     podmansh \
-#     wezterm \
-#     stow \
-#     podman-tui \
-#     podman-compose \
-#     docker-ce \
-#     docker-ce-cli \
-#     docker-buildx-plugin \
-#     docker-compose-plugin \
-#     containerd.io
+rpm-ostree --idempotent install \
+    adobe-source-han-sans-jp-fonts \
+    adobe-source-han-serif-jp-fonts \
+    mozilla-fira-mono-fonts \
+    powerline-fonts \
+    jetbrains-mono-fonts-all \
+    google-droid-sans-mono-fonts \
+    rofi-wayland \
+    kvantum \
+    kvantum-qt5 \
+    qt6ct \
+    qt5ct \
+    onedrive \
+    zathura \
+    zathura-pdf-mupdf \
+    zathura-djvu \
+    zathura-ps \
+    zathura-cb \
+    fcitx5 \
+    fcitx5-configtool \
+    fcitx5-gtk \
+    fcitx5-qt5 \
+    fcitx5-mozc \
+    neovim \
+    eza \
+    ripgrep \
+    zoxide \
+    tmux \
+    iotop \
+    tokei \
+    podmansh \
+    wezterm \
+    stow \
+    podman-tui \
+    podman-compose \
+    podman-docker
 
 # Hyprland Stuff
 rpm-ostree --idempotent install \
@@ -76,12 +71,11 @@ rpm-ostree --idempotent install \
     kitty # For until wezterm wayland stuff is fixed
 
 # Cosmic
-curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
-rpm-ostree --idempotent install cosmic-desktop
+# curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
+# rpm-ostree --idempotent install cosmic-desktop
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
-# systemctl enable docker
 sudo systemctl disable tailscaled.service
 
 curl -Lo /usr/bin/kmonad https://github.com/kmonad/kmonad/releases/latest/download/kmonad

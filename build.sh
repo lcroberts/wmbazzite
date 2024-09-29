85143f0a5285b2723fc78c5e0544cabb83d092dd
Logan Roberts <logancroberts@outlook.com>
2024-09-29 10:00:27 -0400

    Remove cosmic

--- build.sh
+++ build.sh
@@ -71,8 +71,8 @@ rpm-ostree --idempotent install \
     blueman
 
 # Cosmic
-curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
-rpm-ostree --idempotent install cosmic-desktop
+# curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"${RELEASE}"/ryanabx-cosmic-epoch-fedora-"${RELEASE}".repo
+# rpm-ostree --idempotent install cosmic-desktop
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
